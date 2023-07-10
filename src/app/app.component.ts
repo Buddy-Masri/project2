a2b87a84abfbd1e187aa4ab5f9929130886255e0
“Buddy-Masri” <abdel.rahman.almasri@gmail.com>
2023-07-11 00:21:30 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
 
+
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project2';
+
+
+
+
+
+
+
+export class AppComponent     {
+
+  userArray: Array<any> = [];
+  
+
+  name:string;
+  email:string;
+  address:string;
+
+
+
+fx3(){
+  if (this.name != null && this.email != null && this.address != null){
+  this.userArray.push({
+    'name': this.name, 
+    'email':this.email, 
+    'address':this.address})
+  this.name= null
+  this.email= null
+  this.address= null
+  console.log(this.userArray)
+ }
+  else{
+    return
+  }
 }
+
+
+
+fx4(i){
+  this.userArray.splice(i, 1)
+  }
+
+
+
+  
+
+
+
+  title = 'project2'
+}
+
+
+
